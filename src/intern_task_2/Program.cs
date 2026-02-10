0134727bf76adca23df8101be07338472ab2c974
adam <adamducken@gmail.com>
2026-02-10 14:32:09 +0200

    initial commit: added boilerplate stuff and main entry

---
+++ src/intern_task_2/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using intern_task_2.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure in-memory database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("InternTask2Database"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+// Make the Program class public for testing
+public partial class Program { }
