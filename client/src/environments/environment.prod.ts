46a95ac736dc31213742f7dde53a22a091d51c7a
adam <adamducken@gmail.com>
2026-02-25 11:44:57 +0200

    feat(env): add google client id to environments

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7045/api'
+  apiUrl: 'https://localhost:7045/api',
+  googleClientId: '128732564637-0v0sbhaus0nsn1f57q67bg65jmkfg9c6.apps.googleusercontent.com'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5291/api'
+  apiUrl: 'http://localhost:5291/api',
+  googleClientId: '128732564637-0v0sbhaus0nsn1f57q67bg65jmkfg9c6.apps.googleusercontent.com'
 };
