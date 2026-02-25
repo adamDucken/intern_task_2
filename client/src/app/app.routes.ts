a1aa5afb138fd4b921a5d9b9451afaeb2f8003f1
adam <adamducken@gmail.com>
2026-02-25 11:45:53 +0200

    feat(app): wire interceptor, auth guard on routes, navbar, google gsi script

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { AllHousesComponent } from './components/all-houses/all-houses.component';
 import { HouseDetailComponent } from './components/house-detail/house-detail.component';
 import { ApartmentDetailComponent } from './components/apartment-detail/apartment-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/all-houses', pathMatch: 'full' },
-  { path: 'all-houses', component: AllHousesComponent },
-  { path: 'house/:id', component: HouseDetailComponent },
-  { path: 'apartment/:id', component: ApartmentDetailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'all-houses', component: AllHousesComponent, canActivate: [authGuard] },
+  { path: 'house/:id', component: HouseDetailComponent, canActivate: [authGuard] },
+  { path: 'apartment/:id', component: ApartmentDetailComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '/all-houses' }
 ];
